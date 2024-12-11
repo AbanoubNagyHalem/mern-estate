8d7a38c9fdf718397b3920b0691e61ec43152618
Abanoub Nagy <96650563+AbanoubNagyHalem@users.noreply.github.com>
2024-12-11 23:32:47 +0200

    create signup api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -16,9 +17,11 @@ mongoose
   });
 
 const app = express();
+app.use(express.json())
 
 app.listen(3000, () => {
   console.log("Server is running");
 });
 
 app.use("/user", userRouter);
+app.use("/auth", authRouter);
